bd9fa5fb57ef17a4087510906bffe0ea8799ccb5
Urs Thuermann <urs@isnogud.escape.de>
2006-04-24 14:14:09 +0000

    Initial import of llcf-2.0-pre2

---
+++ tst-proc.c
@@ -0,0 +1,120 @@
+/*
+ *  $Id: tst-proc.c,v 2.0 2006/04/13 10:37:21 ethuerm Exp $
+ */
+
+/*
+ * tst-proc.c
+ *
+ * Copyright (c) 2002-2005 Volkswagen Group Electronic Research
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, the following disclaimer and
+ *    the referenced file 'COPYING'.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Volkswagen nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * Alternatively, provided that this notice is retained in full, this
+ * software may be distributed under the terms of the GNU General
+ * Public License ("GPL") version 2 as distributed in the 'COPYING'
+ * file from the main directory of the linux kernel source.
+ *
+ * The provided data structures and external interfaces from this code
+ * are not restricted to be used by modules with a GPL compatible license.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * Send feedback to <llcf@volkswagen.de>
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/uio.h>
+
+#include "af_can.h"
+#include "raw.h"
+
+#define MAX_RAW 800
+
+int main(int argc, char **argv)
+{
+    int s[MAX_RAW];
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+    int i,numsock;
+
+    if (argc != 2) {
+	fprintf(stderr, "Error: Wrong number of arguments. Try %s <number of created sockets>.\n", argv[0]);
+	exit(1);
+    }
+
+    numsock = atoi(argv[1]);
+
+    if (numsock >= MAX_RAW) {
+	fprintf(stderr, "Error: more than %d sockets to open (see #define MAX_RAW).\n", MAX_RAW);
+	exit(1);
+    }
+
+    printf("\ncreating %d raw sockets ... ", numsock);
+
+    if (numsock) {
+	for (i=0; i < numsock; i++) {
+	    if ((s[i] = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("socket");
+		return 1;
+	    }
+
+	    addr.can_family = PF_CAN;
+	    strcpy(ifr.ifr_name, "vcan2");
+	    ioctl(s[i], SIOCGIFINDEX, &ifr);
+	    addr.can_ifindex = ifr.ifr_ifindex;
+
+	    if (bind(s[i], (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("connect");
+		return 1;
+	    }
+	}
+    }
+
+    printf("done.\n");
+
+    printf("Waiting for keyboard input ...");
+
+    getchar();
+
+    printf("closing %d raw sockets ... ", numsock);
+
+    if (numsock)
+	for (i=0; i < numsock; i++)
+	    close(s[i]);
+
+    printf("done.\n\n");
+
+    return 0;
+
+}
---
+++ tst-raw.c
@@ -0,0 +1,142 @@
+/*
+ *  $Id: tst-raw.c,v 2.0 2006/04/13 10:37:21 ethuerm Exp $
+ */
+
+/*
+ * tst-raw.c
+ *
+ * Copyright (c) 2002-2005 Volkswagen Group Electronic Research
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, the following disclaimer and
+ *    the referenced file 'COPYING'.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of Volkswagen nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * Alternatively, provided that this notice is retained in full, this
+ * software may be distributed under the terms of the GNU General
+ * Public License ("GPL") version 2 as distributed in the 'COPYING'
+ * file from the main directory of the linux kernel source.
+ *
+ * The provided data structures and external interfaces from this code
+ * are not restricted to be used by modules with a GPL compatible license.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * Send feedback to <llcf@volkswagen.de>
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#include "af_can.h"
+#include "raw.h"
+
+int main(int argc, char **argv)
+{
+    int s;
+    struct sockaddr_can addr;
+    struct can_filter rfilter[4];
+    struct can_frame frame;
+    int nbytes, i;
+    struct ifreq ifr;
+    char *ifname = "vcan2";
+    int ifindex;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "i:")) != -1) {
+        switch (opt) {
+        case 'i':
+	    ifname = optarg;
+            break;
+        default:
+            fprintf(stderr, "Unknown option %c\n", opt);
+            break;
+        }
+    }
+
+
+    if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+      perror("socket");
+      return 1;
+    }
+
+    rfilter[0].can_id   = 0x123;
+    rfilter[0].can_mask = CAN_SFF_MASK;
+    rfilter[1].can_id   = 0x200;
+    rfilter[1].can_mask = 0x700;
+    rfilter[2].can_id   = 0x80123456;
+    rfilter[2].can_mask = 0x1FFFF000;
+    rfilter[3].can_id   = 0x80333333;
+    rfilter[3].can_mask = CAN_EFF_MASK;
+
+    setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter));
+
+    strcpy(ifr.ifr_name, ifname);
+    ioctl(s, SIOCGIFINDEX, &ifr);
+    ifindex = ifr.ifr_ifindex;
+
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifindex;
+
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+      perror("bind");
+      return 1;
+    }
+
+    while (1) {
+
+	if ((nbytes = read(s, &frame, sizeof(struct can_frame))) < 0) {
+	    perror("read");
+	    return 1;
+	} else if (nbytes < sizeof(struct can_frame)) {
+	    fprintf(stderr, "read: incomplete CAN frame\n");
+	    return 1;
+	} else {
+	    if (frame.can_id & CAN_EFF_FLAG)
+		printf("%8X  ", frame.can_id & CAN_EFF_MASK);
+	    else
+		printf("%3X  ", frame.can_id & CAN_SFF_MASK);
+	    
+	    printf("[%d] ", frame.can_dlc);
+	    
+	    for (i = 0; i < frame.can_dlc; i++) {
+		printf("%02X ", frame.data[i]);
+	    }
+	    if (frame.can_id & CAN_RTR_FLAG)
+		printf("remote request");
+	    printf("\n");
+	    fflush(stdout);
+	}
+    }
+
+    close(s);
+
+    return 0;
+}
