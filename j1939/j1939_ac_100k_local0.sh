f27ee7e50ba1ebdd433126512f9b5542199a57f3
Oleksij Rempel <o.rempel@pengutronix.de>
2019-08-28 08:48:51 +0200

    add j1939 tests
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

---
+++ j1939/j1939_ac_100k_local0.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019 Oleksij Rempel <entwicklung@pengutronix.de>
+
+set -e
+
+CAN0=${1:-can0}
+CAN1=${2:-can1}
+
+dd if=/dev/urandom of=/tmp/test_100k bs=100K count=1
+
+echo "start rx j1939acd and j1939cat on ${CAN0}"
+j1939acd -r 100,80-120 -c /tmp/11223344.j1939acd 11223344 ${CAN0} &
+PID_JACD0=$!
+echo $PID_JACD0
+sleep 2
+j1939cat ${CAN0}:,,0x11223344 -r > /tmp/blup &
+PID_JCAT0=$!
+echo $PID_JCAT0
+
+echo "start tx j1939acd and j1939cat on ${CAN0}"
+j1939acd -r 100,80-120 -c /tmp/11223340.j1939acd 11223340 ${CAN0} &
+PID_JACD1=$!
+sleep 2
+j1939cat -i /tmp/test_100k ${CAN0}:,,0x11223340 :,,0x11223344
+sleep 2
+
+echo "kill all users on ${CAN0}"
+kill $PID_JACD0
+kill $PID_JCAT0
+kill $PID_JACD1
+
+cmp /tmp/test_100k /tmp/blup
+
+exit $?
