d2cbeb0e0c20ecd22bec414a2d48e870874b0687
Marc Kleine-Budde <mkl@pengutronix.de>
2021-05-27 15:55:04 +0200

    j1939: test j1939 module usage for proper cleanup
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- j1939/run_all.sh
+++ j1939/run_all.sh
@@ -4,8 +4,15 @@ set -e
 
 for f in j1939*.sh; do
 	echo "##############################################"
+	pre=$(lsmod | awk '/^can_j1939/ { print $3 }')
 	echo "run: $f"
 	./$f "${@}"
 	echo "done: $f"
+	post=$(lsmod | awk '/^can_j1939/ { print $3 }')
+	if [ $pre -ne $post ]; then
+		echo "module usage: before start: $pre"
+		echo "              after finish: $post"
+		exit 1
+	fi
 	echo "##############################################"
 done
